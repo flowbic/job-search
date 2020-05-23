f0211e625720b83d214484931d59135aab3c33c6
joelcarlss <jc222mw@student.lnu.se>
2020-05-23 14:48:31 +0200

    first

---
+++ server/app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const helmet = require('helmet')
+const port = process.env.PORT || 3000
+const bodyParser = require('body-parser')
+var cors = require('cors')
+
+
+const app = express()
+
+// Adds cors header so webpages can access
+app.use(cors())
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// Helmet for security
+app.use(helmet())
+
+// routes
+require('./routes/')(app)
+
+app.listen(port || 3000, () => {
+    console.log(`running on localhost:${port}`)
+    console.log('Press Ctrl-C to terminate...')
+})
